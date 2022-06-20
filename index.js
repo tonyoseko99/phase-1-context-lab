8d93dd2127e218c3c341b1721e167f3279ef99dc
Tonny Oseko <tonnyoseko0@gmail.com>
2022-06-20 22:36:52 +0300

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
+ const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -21,3 +21,60 @@ const allWagesFor = function () {
     return payable
 }
 
+const createEmployeeRecord = function (recArray){
+    return {
+        firstName: recArray[0],
+        familyName: recArray[1],
+        title: recArray[2],
+        payPerHour: recArray[3],
+        timeInEvents:[],
+        timeOutEvents:[]
+    };
+};
+
+const createEmployeeRecords = function (recsArray){
+    return recsArray.map(rec=>createEmployeeRecord(rec));
+};
+
+const createTimeInEvent = function (datestamp){
+    let [date, hour] = datestamp.split(' ');
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date: date
+    });
+    return this  
+};
+
+const createTimeOutEvent = function (datestamp){
+    let [date, hour] = datestamp.split(' ');
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date: date
+    });
+    return this  
+};
+
+const hoursWorkedOnDate = function (date){
+    const inEvent = this.timeInEvents.find(inEvent=>inEvent.date === date);
+    const outEvent = this.timeOutEvents.find(outEvent=>outEvent.date === date);
+            return ((outEvent.hour - inEvent.hour)/100)
+};
+
+const wagesEarnedOnDate = function (date){
+    return hoursWorkedOnDate.apply(this, [date])* this.payPerHour
+};
+
+const findEmployeeByFirstName = function (srcArray, targetName){
+    for(let i=0; i<srcArray.length;i++){
+        if(srcArray[i].firstName===targetName){
+            return srcArray[i]
+        }}
+};
+
+const calculatePayroll = function(recsArray){
+    let payroll = []
+    recsArray.forEach(element => payroll.push(allWagesFor.call(element)))
+    return payroll.reduce((a,b)=>{return a+b})
+}
